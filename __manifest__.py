0ab918cab6b56d895a031920050392b92245ae9f
jmusun12 <stdjosemusun@gmail.com>
2021-04-08 09:34:35 -0600

    Construir modulo

---
+++ __manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'Sitio Imagen',
+    'description': 'Modulo para definir el sitio web de chicos',
+    'summary': '',
+    'author': 'Javier Garcia',
+    'version': '13.0',
+    'depends': [
+        'website', 'website_sale'
+    ],
+    'data': [
+        'views/assets.xml',
+        'views/header.xml',
+        'views/footer.xml',
+        'views/home.xml'
+    ],
+    'auto_install': False,
+    'installable': True,
+    'license': 'GPL-1',
+}
